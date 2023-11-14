cc366816e167388b9bd8321cf461be0415eaaa6b
Sowndhar <sowndharnewbie@gmail.com>
2023-11-14 11:00:03 +0530

    Client-server

---
+++ ClientServerCommunication/ClientApplication/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientApplication
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string serverIp="127.0.0.1";
+            int port = 8080;
+            try
+            {
+                TcpClient client = new TcpClient(serverIp,port);
+                Console.WriteLine("Connected to the server");
+
+                NetworkStream stream= client.GetStream();
+
+                while (true)
+                {
+                    Console.WriteLine("Enter a msg: ");
+                    string msg =Console.ReadLine();
+                    byte[] data = Encoding.ASCII.GetBytes(msg);
+                    stream.Write(data, 0, data.Length);
+
+                    if (msg.ToLower() == "close")
+                    {
+                        Console.WriteLine("Closing the connection as requested.");
+                        break;
+                    }
+
+                    byte[] buffer = new byte[256];
+                    int bytesRead = stream.Read(buffer, 0, buffer.Length);
+                    string response = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+                    Console.WriteLine($"Received:{response}");
+                }
+
+                client.Close();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ClientServerCommunication/ServerApplication/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+namespace ConsoleApplication
+{
+ 
+    class Program
+    {
+        
+        public static void Main(string[] args)
+        {
+            TcpListener server = null;
+            try
+            {
+                int port = 8080;
+                //IPAddress localAddress = IPAddress.Parse("127.0.0.1");
+                IPAddress localAddress = IPAddress.Any;
+
+                server = new TcpListener(localAddress, port);
+
+                server.Start();
+
+                while (true)
+                {
+                    TcpClient client = server.AcceptTcpClient();
+                    Console.WriteLine("Client connected");
+
+                    NetworkStream stream = client.GetStream();
+
+                    while (true)
+                    {
+
+                        byte[] data = new byte[256];
+                        int bytesRead = stream.Read(data, 0, data.Length);
+                        string message = Encoding.ASCII.GetString(data, 0, bytesRead);
+                        Console.WriteLine($"Received:{message}");
+
+                        if (message.ToLower()=="close")
+                        {
+                            Console.WriteLine("Closing the connection");
+                            break;
+                        }
+                        Console.WriteLine("Enter a response: ");
+                        string serverInput=Console.ReadLine();
+                        byte[] response = Encoding.ASCII.GetBytes(serverInput);
+                        stream.Write(response, 0, response.Length);
+                    }
+                    client.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                server.Stop();
+                Console.ReadKey();
+            }
+        }
+
+       
+
+    }
+}
